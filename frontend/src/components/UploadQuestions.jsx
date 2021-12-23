812eede3546a405151930c2b4d330bca91416b67
dreadwing5 <sachinspyk@outlook.com>
2021-12-23 23:04:06 +0530

    Removed tailwind, added mui

--- frontend/src/components/UploadQuestions.jsx
+++ frontend/src/components/UploadQuestions.jsx
@@ -1,92 +1,50 @@
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { React, useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Container } from "@mui/material";
 
+import MCQs from "./MCQs";
+import Subjective from "./Subjective";
+import Others from "./OtherQuestion";
 
-import "./UploadQuestions.css";
-import  MCQs  from "./MCQs";
-import  Subjective  from "./Subjective";
-import  Others  from "./OtherQuestion";
+const UploadQuestions = () => {
+  const quesType = [
+    { id: 1, name: "MCQs" },
+    { id: 2, name: "Subjective" },
+    { id: 3, name: "Others" },
+  ];
 
-  const UploadQuestions = () => {
-    const quesType = [
-      { id: 1, name: "MCQs" },
-      { id: 2, name: "Subjective" },
-      { id: 3, name: "Others" },
-    ];
-    const [selectedQuesType, setSelectedQuesType] = useState(quesType[0]);
+  const [questionType, setQuestionType] = useState(quesType[0].name);
+
+  const handleChange = (event) => {
+    setQuestionType(event.target.value);
+  };
 
   return (
-    <Fragment>
-    <div className='heading'>
-    <h1>Upload Questions</h1>
-    </div>
-    <div className="w-55 text-right fixed top-16 ">
-    <div className="px-1 py-1">
-    <div className="w-72 fixed top-16">
-     <Listbox value={selectedQuesType} onChange={setSelectedQuesType}>
-     <div className="relative mt-1">
-        <Listbox.Label>QuestionType:</Listbox.Label>
-        
-        <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-
-        <span className="block truncate">{selectedQuesType.name}</span>
-            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-            <SelectorIcon
-                className="w-5 h-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </span>
-            </Listbox.Button>
-            <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+    <Container maxWidth="sm">
+      <Box m={2} pt={3}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Question Type</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={questionType.name}
+            label="Question Type"
+            onChange={handleChange}
           >
-
-<Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-          {quesType.map((type) => (
-            <Listbox.Option key={type.id} value={type}  className={({ active }) =>
-            `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
-                  cursor-default select-none relative py-2 pl-10 pr-4`
-          }>
-              {({ selectedQuesType, active }) => (
-                    <>
-                      <span
-                        className={`${
-                          selectedQuesType ? 'font-medium' : 'font-normal'
-                        } block truncate`}
-                      >
-                        {type.name}
-                      </span>
-                      {selectedQuesType ? (
-                        <span
-                          className={`${
-                            active ? 'text-amber-600' : 'text-amber-600'
-                          }
-                                absolute inset-y-0 left-0 flex items-center pl-3`}
-                        >
-                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-        </Transition>
-        </div>
-      </Listbox>
-      </div>
-            </div>
-    </div>
-    {selectedQuesType.name === "MCQs" && <MCQs />}
-      {selectedQuesType.name === "Subjective" && <Subjective />}
-      {selectedQuesType.name === "Others" && <Others />}
-    </Fragment>
-  )
-}
+            {quesType.map((ques) => (
+              <MenuItem key={ques.id} value={ques.name}>
+                {ques.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </Container>
+  );
+};
 
 export default UploadQuestions;
-
