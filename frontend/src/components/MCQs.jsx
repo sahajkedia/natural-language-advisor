311850b1bd130aaeb7718a60b3c3fe684040b24e
Zoheb Alam <zohebzob@gmail.com>
2021-12-23 21:46:26 +0530

    Co-authored-by: Sachin Kumar <sachinspyk@outlook.com>

---
+++ frontend/src/components/MCQs.jsx
@@ -0,0 +1,15 @@
+
+
+
+const MCQs = () => {
+    return (
+      <div>
+        <label htmlFor="">Question</label>
+        <input type="text" name="Question" id="" />
+        <textarea name="" id="" cols="50" rows="2"></textarea>
+        <h1>MCQs</h1>
+      </div>
+    );
+  };
+  
+  export default MCQs;
---
+++ frontend/src/components/OtherQuestion.jsx
@@ -0,0 +1,9 @@
+const OtherQuestion = () => {
+    return (
+      <div>
+        <h1>Others</h1>
+      </div>
+    );
+  };
+  
+  export default OtherQuestion;
