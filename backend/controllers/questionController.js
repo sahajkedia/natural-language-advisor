606f81b6961128db3a0728d92d1aeae9a30bfe89
dreadwing5 <sachinspyk@outlook.com>
2021-12-24 10:34:11 +0530

    Added question routes

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,117 +1,42 @@
-const { v4: uuidv4 } = require("uuid");
-const Student = require("../models/StudentSchema");
-const MCQuestion = require("../models/MCQuestionSchema");
-const RiddleQuestion = require("../models/RiddleSchema");
-const StudentSignup = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  const userExists = Student.findOne({ email: email });
-  if (userExists) {
-    res.status(404).json({
-      message: "Not able to Sign up!",
-    });
-  } else {
-    const newStudent = new Student({
-      name,
-      email,
-      password,
-    });
+const Question = require("../models/Question");
 
-    try {
-      await newStudent.save();
-      res.status(201).json({
-        details: newStudent,
-      });
-    } catch (e) {
-      res.status(400).json({
-        message: "could not complete your request",
-      });
-      return next(e);
-    }
-  }
-};
-
-const MCQuestionAdding = (req, res, next) => {
-  const { option1, option2, option3, option4, answer, question } = req.body;
-
+exports.getAllQuestions = async (req, res) => {
   try {
-    let quizQs = new MCQuestion({
-      id: "Q0",
-      qs: question,
-      options: [option1, option2, option3, option4],
-      ans: answer,
-    });
-    quizQs.save();
-
-    console.log(quizQs);
-    res.status(200).json({
-      message: "Question Saved",
-    });
-  } catch (c) {
-    res.status(404).json({
-      message: "Not Possible to Save Successfully",
-    });
+    const questions = await Question.find();
+    res.json(questions);
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
 
-const QuizPage = async (req, res, next) => {
-  const circularReplacer = () => {
-    // Creating new WeakSet to keep
-    // track of previously seen objects
-    const seen = new WeakSet();
-
-    return (key, value) => {
-      // If type of value is an
-      // object or value is null
-      if (typeof value === "object" && value !== null) {
-        // If it has been seen before
-        if (seen.has(value)) {
-          return;
-        }
-
-        // Add current value to the set
-        seen.add(value);
-      }
-
-      // return the value
-      return value;
-    };
-  };
-  let qs = await MCQuestion.find({});
-  console.log(qs);
-  //  JSON.stringify(qs,circularReplacer()).toArray( function(err, results) {
-  //     console.log(results)
-  //  });
-  res
-    .json({
-      questions: qs,
-    })
-    .status(201);
-};
-
-const RiddlesAdding = (req, res, next) => {
-  const { answer, qsname } = req.body;
-
+exports.createQuestion = async (req, res) => {
   try {
-    let RiddleQs = new RiddleQuestion({
-      id: uuidv4(),
-      qs: qsname,
-      // options:[option1,option2,option3,option4],
-      ans: answer,
-    });
-    RiddleQs.save();
+    console.log(req.body);
 
-    console.log(RiddleQs);
-    res.status(200).json({
-      message: "Riddle Saved "
-    });
-  } catch (c) {
-    res.status(404).json({
-      message: "Not Possible to Save Successfully",
-    });
+    // const question = await Question.create(req.body);
+    // res.json(question);
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
 
-exports.StudentSignup = StudentSignup;
-exports.MCQuestionAdding = MCQuestionAdding;
-exports.QuizPage = QuizPage;
-exports.RiddlesAdding = RiddlesAdding;
+exports.updateQuestion = async (req, res) => {
+  try {
+    const question = await Question.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.json(question);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+exports.deleteQuestion = async (req, res) => {
+  try {
+    const question = await Question.findByIdAndDelete(req.params.id);
+    res.json(question);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
