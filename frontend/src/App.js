91b96d29ddf2f235c285bd385a7a758c2af99972
dreadwing5 <sachinspyk@outlook.com>
2021-12-30 16:15:51 +0530

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AssessmentForm from "./components/Assessmentform";
 import LoadQuestions from "./components/LoadQuestion";
+import Video from "./components/Video";
 import NewQuestionForm from "./components/NewQuestionForm";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ThemeConfig from "./theme";
@@ -15,6 +16,7 @@ export default function App() {
           <Route exact path="/" component={NewQuestionForm} />
           <Route path="/take-assessment" component={AssessmentForm} />
           <Route path="/load-questions" component={LoadQuestions} />
+          <Route path="/video" component={Video} />
         </Switch>
       </BrowserRouter>
     </ThemeConfig>
--- frontend/src/components/LoadQuestion.jsx
+++ frontend/src/components/LoadQuestion.jsx
@@ -1,6 +1,6 @@
-import "./LoadQuestion.css"
+import "./LoadQuestion.css";
 import * as Yup from "yup";
-import * as React from 'react';
+import * as React from "react";
 
 import { useState, useCallback, forwardRef } from "react";
 import { Form, FormikProvider, useFormik } from "formik";
@@ -30,64 +30,52 @@ const LabelStyle = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(1),
 }));
 
-
-
-
 export default function LoadQuestion() {
-
-    return (
-        <>
-        <div class="wrapper" id="app">
-      <div class="card-form">
-        <div class="card-list">
-          <div class="card-item -front">
-            <div class="card-item__side">
-              <div class="card-item__cover">
-                <img src="https://www.allianz.ie/assets/img/illustration/248h/man-asking-question.png"class="card-item__bg"/>
+  return (
+    <>
+      <div class="wrapper" id="app">
+        <div class="card-form">
+          <div class="card-list">
+            <div class="card-item -front">
+              <div class="card-item__side">
+                <div class="card-item__cover">
+                  <img
+                    src="https://www.allianz.ie/assets/img/illustration/248h/man-asking-question.png"
+                    class="card-item__bg"
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="card-form__innerr">
-          <p>What is the name of the first president (SAMPLE QUESTION)</p>
-
-
+          <div class="card-form__innerr">
+            <p>What is the name of the first president (SAMPLE QUESTION)</p>
 
             <div>
-          <LabelStyle>Write your Answer Below:-</LabelStyle>
+              <LabelStyle>Write your Answer Below:-</LabelStyle>
 
+              <Box
+                component="form"
+                sx={{
+                  "& > :not(style)": { m: 1, width: "50ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="standard-basic"
+                  label="Multiline Answer"
+                  variant="standard"
+                  multiline
+                />
+              </Box>
+            </div>
 
-          <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '50ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      
-      <TextField id="standard-basic" label="Multiline Answer" variant="standard" multiline/>
-    </Box>
-
-
-
-
-                  
-              
-                </div>
-              
-
-
-
-
-          <button style={{margin:"2rem 0"}}class="card-form__button">Submit</button>
+            <button style={{ margin: "2rem 0" }} class="card-form__button">
+              Submit
+            </button>
+          </div>
         </div>
       </div>
-
-    </div>
-        </>
-    )
-
-
-
-};
+    </>
+  );
+}
